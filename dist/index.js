bcafd284af5c98727c804e8e6c0273658608f424
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-28 18:03:06 +0530

    Set up basic WebSocket server connection logging

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello");
-console.log("hi");
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+console.log("WebSocket server started on port 8080");
+let userCount = 0;
+wss.on("connection", (socket) => {
+    userCount++;
+    console.log("User Connected #" + userCount);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import {WebSocketServer}from 'ws';
-const wss = new WebSocketServer({port : 8080});
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+console.log("WebSocket server started on port 8080");
+
+let userCount = 0;
+
+wss.on("connection", (socket) => {
+    userCount++;
+    console.log("User Connected #" + userCount);
+});
