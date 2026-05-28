f204390f5c2175c8800a365c24cbadd5ba0dfa12
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-28 21:39:38 +0530

    improved and made it to broadcast to all the users connected to the room

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,17 @@ const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 console.log("WebSocket server started on port 8080");
 let userCount = 0;
+let allSockets = [];
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     userCount++;
     console.log("User Connected #" + userCount);
     socket.on("message", (message) => {
         console.log("Message recived" + message.toString());
-        socket.send(message.toString() + "sent from server");
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            socket.send(message.toString() + "sent from server");
+        }
     });
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer ,WebSocket} from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
 console.log("WebSocket server started on port 8080");
 
 let userCount = 0;
-
+let allSockets: WebSocket[] = [];
 wss.on("connection", (socket) => {
+    allSockets.push(socket)
     userCount++;
     console.log("User Connected #" + userCount);
 
     socket.on("message",(message) => {
         console.log("Message recived" + message.toString());
-        socket.send(message.toString() + "sent from server");
+        for(let i = 0;i<allSockets.length;i++){
+            const s = allSockets[i];
+            socket.send(message.toString() + "sent from server");
+        }
     })
 });
