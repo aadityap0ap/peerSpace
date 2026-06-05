dee6687a3ff4a898541cd867ae57ccf5fd23954d
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-05 18:29:47 +0530

    signup and signin routes added

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express  from "express";
+import authRouter from "./routes/auth"
+import { ConnectDB } from "./db/db";
+import { configDotenv } from "dotenv";
+configDotenv();
+const app = express();
+
+
+app.use(express.json());
+app.use("/auth",authRouter);
+
+
+async function StartServer(){
+    try{
+        await ConnectDB();
+        app.listen(3000,() => {
+            console.log("Server running on port 3000")
+        })
+    }
+    catch(error){
+        console.log("DB error Occured!")
+    }
+}
+StartServer();
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,2 +1,44 @@
 import {Router } from "express";
+import { User } from "../db/db";
+import { runInNewContext } from "node:vm";
 const router = Router()
+
+router.post("/signup",async(req,res) => {
+    try{
+        const {username,password} = req.body;
+        await User.create({
+            username,
+            password
+        });
+        return res.status(200).json({
+            message:"Account Created Successfully!"
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            message:"Backend Error ! && Server Crahsed !"
+        })
+    }
+})
+
+router.post("/signin",async(req,res) => {
+    try{
+        const {username,password} = req.body;
+        const find = await User.findOne({password});
+        if(!find){
+            return res.status(400).json({
+                message:"Invalid PassWord!"
+            });
+        }
+        return res.status(200).json({
+            message : `Welcome to PeerSpace ${username}`
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            message:"Backend Erorr || Server Crahsed !"
+        })
+    }
+})
+
+export default router;
