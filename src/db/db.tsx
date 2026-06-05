585d3b9fa8226c6fe9b5fcdd6876618e76edd990
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-05 17:46:30 +0530

    Added connectDB function in db.ts that will make sure the db connection is extablished before starting the server

--- src/db/db.tsx
+++ src/db/db.tsx
@@ -1,5 +1,32 @@
 import mongoose from "mongoose";
 
+export async function ConnectDB(){
+    await mongoose.connect(process.env.MONGO_URL!);
+//     /*
+// Why use the "!" operator with process.env.MONGO_URL?
+
+// In TypeScript, environment variables are typed as
+// string | undefined because TypeScript cannot guarantee
+// that a variable exists in the .env file.
+
+// However, mongoose.connect() expects a value of type string.
+
+// By writing:
+
+//     process.env.MONGO_URL!
+
+// the non-null assertion operator (!) tells TypeScript:
+
+//     "I am sure this value exists and is not undefined."
+
+// This removes the type error and allows the value to be
+// passed to mongoose.connect().
+
+// The "!" should only be used when the environment variable
+// is guaranteed to exist, typically after loading the .env
+// file using dotenv.config().
+// */
+}
 
 const userSchema = new mongoose.Schema({
     username : String,
