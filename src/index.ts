e13987fba56e56874e037b939b2c9519e5d1018d
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-05 22:21:47 +0530

    Cors added for syncing backend and frontend

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import express  from "express";
 import authRouter from "./routes/auth"
 import { ConnectDB } from "./db/db";
 import { configDotenv } from "dotenv";
+import cors from "cors";
 configDotenv();
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/auth",authRouter);
 
