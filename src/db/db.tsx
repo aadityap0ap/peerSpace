dac684d6b65ab687ffcda7dc862a336adcda1883
aadityap0ap <adityabharti450ap@gmail.com>
2026-06-14 17:48:39 +0530

    Created a friendrequest Model

--- src/db/db.tsx
+++ src/db/db.tsx
@@ -58,7 +58,29 @@ const userSchema = new mongoose.Schema({
 const roomSchema = new mongoose.Schema({
     roomId : String,
     createdBy : String
-})
+});
+
+const friendRequestSchema = new mongoose.Schema({
+    sender :{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required : true
+        },
+    receiver :{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            required : true
+        },
+    status : {
+        type : String,
+        enum : ["pending","accepted","rejected"],
+        default : "pending"
+    }    
+},
+{
+    timestamps: true,
+}
+);
 
 export const User = mongoose.model("User",userSchema);
 export const room = mongoose.model("room",roomSchema);
